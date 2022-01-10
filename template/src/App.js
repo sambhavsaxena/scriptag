28d8e81f6bfacc1c905fe59a246afacacf9f38a2
Sambhav Saxena <76242518+sambhavsaxena@users.noreply.github.com>
2022-01-10 19:51:41 +0530

    Add files via upload

---
+++ template/src/App.js
@@ -0,0 +1,23 @@
+import './style.css'
+
+function App() {
+  return (
+    <div className="App body">
+      <h1 className="name">Your Name</h1>
+      <p className='content'>Hey, I'm a Senior Software Engineer at <a className='link' href="https://company.com">Company</a>. I enjoy working with javascript and crafting beautiful front-end experiences.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+
+      </p>
+      <p className='content'>
+        <strong>Following are some of my projects and experiences.</strong>
+        <br /><br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est aut natus ipsum. Ipsum incidunt error eos exercitationem ea. Sit aliquid tempore exercitationem ratione provident itaque est nesciunt rem dicta expedita deserunt fugiat ea assumenda delectus tenetur ipsum, sed inventore porro architecto, in animi! Recusandae dolorem perferendis sapiente, porro officia libero blanditiis vero expedita. Soluta ad laborum qui, illo officiis nulla atque aspernatur temporibus beatae deleniti, eligendi repellendus impedit, recusandae vitae ex dignissimos labore. Excepturi odio nostrum est assumenda eligendi tenetur ipsum porro modi dignissimos facere, consectetur aliquam eos dicta natus, ab ad nihil adipisci obcaecati fugiat nesciunt! Magni deserunt eveniet rem porro accusantium. Mollitia enim, itaque ipsum harum adipisci cumque facere libero dolorum quo reiciendis. Rerum voluptates quas amet ducimus explicabo atque voluptatem blanditiis ullam libero!
+      </p>
+      <footer className="footer">
+        <a href="https://github.com/sambhavsaxena" target={"_blank"} rel='noreferrer' className='link'>Download CV</a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
